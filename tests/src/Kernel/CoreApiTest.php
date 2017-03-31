e0014c375e79b5df064d3cfbcd2a24817ea0ff93
Daniel Wehner <dawehner@googlemail.com>
2017-03-31 14:05:40 +0100

    Try to fix the synching publishing

--- tests/src/Kernel/CoreApiTest.php
+++ tests/src/Kernel/CoreApiTest.php
@@ -29,6 +29,11 @@ class CoreApiTest extends KernelTestBase {
   ];
 
   /**
+   * @FIXME Use TRUE here.
+   */
+  protected $strictConfigSchema = FALSE;
+
+  /**
    * {@inheritdoc}
    */
   protected function setUp() {
@@ -59,6 +64,7 @@ class CoreApiTest extends KernelTestBase {
     $this->installEntitySchema('node');
     $this->installEntitySchema('user');
     $this->installConfig('workbench_moderation');
+    $this->installConfig('language');
   }
 
   /**
@@ -107,6 +113,37 @@ class CoreApiTest extends KernelTestBase {
     $published_entity = $storage->load($entity->id());
     $this->assertEquals('en-name--1', $published_entity->label());
     $this->assertEquals('fr-name--1', $published_entity->getTranslation('fr')->label());
+  }
+
+  public function testSyncTranslationRevision() {
+    \Drupal::service('module_installer')->install(['content_translation', 'content_translation_revision']);  
+    $storage = \Drupal::entityTypeManager()->getStorage('node');
+    \Drupal::configFactory()
+      ->getEditable('content_translation_revision.settings')
+      ->set('sync_moderation_state_translations', TRUE)
+      ->save();
+
+    $entity = Node::create([
+      'type' => 'article',
+      'title' => 'en-name--0',
+      'moderation_state' => ['target_id' => 'draft'],
+    ]);
+
+    $entity_fr = $entity->addTranslation('fr', ['moderation_state' => ['target_id' => 'draft']]);
+    $entity_fr->setNewRevision(FALSE);
+    $entity_fr->get('title')->value = 'fr-name--0';
+    $entity_fr->save();
+
+    // Publishing the non default translation should also publish the other.
+    $entity_fr->set('moderation_state', ['target_id' => 'published']);
+    $entity_fr->save();
+
+    $this->assertEquals('published', $entity->get('moderation_state')->target_id);
+    $this->assertTrue($entity->isPublished());
+    $this->assertEquals(1, \Drupal::database()->query('SELECT status from {node_field_data} where nid = :nid and langcode = :langcode', [':nid' => $entity->id(), ':langcode' => 'en'])->fetchField());
+    $this->assertEquals('published', $entity->getTranslation('fr')->get('moderation_state')->target_id);
+    $this->assertTrue($entity->getTranslation('fr')->isPublished());
+    $this->assertEquals(1, \Drupal::database()->query('SELECT status from {node_field_data} where nid = :nid and langcode = :langcode', [':nid' => $entity->id(), ':langcode' => 'fr'])->fetchField());
   }
 
 }
